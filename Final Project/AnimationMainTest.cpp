9ede4dbc5619f8b573d9b1e270cab51e2bc0b987
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-04-09 14:23:42 -0600

    push

---
+++ Final Project/AnimationMainTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by wyatt on 4/9/23.
+//
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 500), "Tic Tac Toe!");
+}
