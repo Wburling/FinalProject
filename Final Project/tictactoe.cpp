b6171cc6640b783dcc74ac3fb0324912d520f595
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-04-10 14:19:17 -0600

    Semi Working

--- Final Project/tictactoe.cpp
+++ Final Project/tictactoe.cpp
@@ -1,9 +1,84 @@
 #include "tictactoe.h"
+#include <cstdlib>
 
+int turnCount = 0;
 void TicTacToe::draw(sf::RenderWindow *window) {
-    std::cout << "Drawn" << std::endl;
     window->draw(mSprite);
     window->draw(mText);
     mSprite.setOrigin(imageSize.x/2, imageSize.y/2);
-    mSprite.setPosition(window->getSize().x/2, window->getSize().y/2);
+}
+void TicTacToe::update(sf::Event& e, sf::RenderWindow& window){
+// get position of the mouse
+sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+
+bool mouseInButton =    mousePosition.x >= mSprite.getPosition().x - mSprite.getGlobalBounds().width/2
+                        && mousePosition.x <= mSprite.getPosition().x + mSprite.getGlobalBounds().width/2
+                        && mousePosition.y >= mSprite.getPosition().y - mSprite.getGlobalBounds().height/2
+                        && mousePosition.y <= mSprite.getPosition().y + mSprite.getGlobalBounds().height/2;
+if(e.type == sf::Event::MouseMoved)
+{
+if(mouseInButton)
+{
+mSprite.setColor(sf::Color::Blue);
+}
+else
+{
+mSprite.setColor(sf::Color::White);
+}
+}
+if (e.type == sf::Event::MouseButtonPressed)
+{
+if(e.mouseButton.button==sf::Mouse::Left)
+{
+if(mouseInButton)
+{
+//mSprite.setScale(0,0);
+turnCount++;
+std::cout << "Pressed" << endl;
+replace(&mSprite);
+}
+else
+{
+mSprite.setRotation(0);
+
+}
+}
+}
+if (e.type == sf::Event::MouseButtonReleased)
+{
+if (e.mouseButton.button==sf::Mouse::Left)
+{
+if(mouseInButton)
+{
+mSprite.setColor(sf::Color::Blue);
+mSprite.setRotation(0);
+}
+else
+{
+mSprite.setColor(sf::Color::White);
+mSprite.setRotation(0);
+}
+}
+}
+}
+
+void TicTacToe::replace(sf::Sprite* sprite) {
+    sf:: Texture& textures = *(new sf::Texture);
+    if (turnCount%2 !=0) {
+        textures.loadFromFile("X.png");
+        sprite->setTexture(textures);
+        sprite->setOrigin(imageSize.x/2, imageSize.y/2);
+        sprite->setScale(1,1);
+        cout << "X" << endl;
+    }
+    else {
+        if((!mTexture.loadFromFile("O.png"))) {
+            std::cout<<"Error opening file\n";
+            exit(1);
+        }
+        textures.loadFromFile("O.png");
+        sprite->setTexture(textures);
+        cout << "O" << endl;
+    }
+
 }
--- Final Project/tictactoe.h
+++ Final Project/tictactoe.h
@@ -6,11 +6,16 @@
 #pragma once
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 using namespace std;
 
 // Class for GUI
 class TicTacToe {
 public:
+
 TicTacToe() : TicTacToe("black.jpg") {}
 TicTacToe(const string& fileName) {
     if (fileName == "black.jpg") {
@@ -29,6 +34,7 @@ TicTacToe(const string& fileName) {
         //choose colors
         mText.setFillColor(sf::Color::White);
     }
+
     if (!mTexture.loadFromFile(fileName))
     {
         std::cout<<"Error opening file\n";
@@ -39,13 +45,29 @@ TicTacToe(const string& fileName) {
     imageSize=mTexture.getSize();
     mSprite.setOrigin(imageSize.x/2, imageSize.y/2);
     mSprite.setScale(1,1);
-
+    mSprite.setPosition(500, 400);
+}
+TicTacToe(const string& fileName, float x, float y, float scaleSquare) {
+    if (!mTexture.loadFromFile(fileName))
+    {
+        std::cout<<"Error opening file\n";
+        exit(1);
+    }
+    mSprite.setTexture(mTexture);
+    //get size of image
+    imageSize=mTexture.getSize();
+    mSprite.setOrigin(imageSize.x/2, imageSize.y/2);
+    mSprite.setScale(scaleSquare,scaleSquare);
+    mSprite.setPosition(x,y);
 }
     void draw(sf::RenderWindow* window);
+    void update(sf::Event& e, sf::RenderWindow& window);
+    void replace(sf::Sprite* sprite);
 private:
     sf::Sprite mSprite;
     sf::Texture mTexture;
     sf::Vector2u imageSize;
     sf::Font mFont;
     sf:: Text mText;
+    bool pressed = false;
 };
