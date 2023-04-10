473efb6ed658f8a1c565c1343dd840d93019ea56
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-04-10 12:58:33 -0600

    Holy

--- Final Project/tictactoe.h
+++ Final Project/tictactoe.h
@@ -7,17 +7,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 using namespace std;
-//Tic Tac Toe basics
-const int NUM_ROWS = 3;
-const int NUM_COLS = 3;
-const int NUM_OF_COORD = 2;
-void initializeBoard(char board[][NUM_COLS]);
-void displayBoard(const char board[][NUM_COLS]);
-void updateBoard(char board[][NUM_COLS], int row, int col, char playerToken);
-bool isValidMove(const char board[][NUM_COLS], int row, int col);
-bool isWin(const char board[][NUM_COLS],char playerToken);
-bool isFull(const char board[][NUM_COLS]);
-void getMove(int player, int move[], char board[][NUM_COLS]);
+
 // Class for GUI
 class TicTacToe {
 public:
