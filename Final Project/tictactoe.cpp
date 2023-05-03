516b0984f6c5fbf05f88edb9dfbe1e95dd4624f0
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-05-03 10:44:50 -0600

    Window Name

--- Final Project/tictactoe.cpp
+++ Final Project/tictactoe.cpp
@@ -12,8 +12,8 @@ void start() {
     turnCount = 0;
     player1.clear();
     player2.clear();
-    //sf::RenderWindow window(sf::VideoMode(1000, 800), "Tic Tac Toe!");
-    sf::RenderWindow window(sf::VideoMode(1000, 800, 32), "window", sf::Style::Titlebar | sf::Style::Close);
+
+    sf::RenderWindow window(sf::VideoMode(1000, 800, 32), "Tic Tac Toe!", sf::Style::Titlebar | sf::Style::Close);
 
     sf::Vector2f Center(1000, 1000);
     sf::Vector2f HalfSize(400, 300);
